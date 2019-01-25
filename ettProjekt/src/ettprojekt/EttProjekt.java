3284065bb52cd4b79d90085e31fbd4160a7b9d5d
Christoffer <Christoffer@10.22.12.57>
2019-01-25 13:38:15 +0100

    Fixat databaskoppling och tom startklass

--- ettProjekt/src/ettprojekt/EttProjekt.java
+++ ettProjekt/src/ettprojekt/EttProjekt.java
@@ -5,18 +5,35 @@
  */
 package ettprojekt;
 
+import javax.swing.JOptionPane;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
 /**
  *
  * @author Christoffer
  */
 public class EttProjekt {
 
+    public static String userDir;
+    public static InfDB idb;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //Test kommentar
+        userDir = System.getProperty("user.dir"); //Hämtar vart programmet körs ifrån
+        userDir += "/lib/DATABASE.FDB"; //Pekar på vart databasen ligger lagrad
+        try {
+            //Importerar databasen
+            idb = new InfDB(EttProjekt.userDir);
+            new start().setVisible(true);//Startar programmet genom att köra valElevLarare.java
+            System.out.println(userDir);
+        } catch (InfException undantag) {
+            JOptionPane.showMessageDialog(null, "Programmet kan ej ansluta till databasen");
+            System.out.println("Internt felmeddelande" + undantag.getMessage());
+            System.out.println(userDir);
+        }
     }
-    
+
 }
