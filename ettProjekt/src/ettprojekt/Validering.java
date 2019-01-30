eec0a67cb3263404314d878c70c25937be477a67
Christoffer <Christoffer@10.22.12.57>
2019-01-30 14:04:47 +0100

    bildmapp

--- ettProjekt/src/ettprojekt/Validering.java
+++ ettProjekt/src/ettprojekt/Validering.java
@@ -5,57 +5,112 @@
  */
 package ettprojekt;
 
+import static ettprojekt.EttProjekt.idb;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import oru.inf.InfException;
 
 /**
  *
  * @author emmaj
  */
 public class Validering {
-    
+
 //Metod för att kontrollera om inmatningsrutan är tom    
-    public static boolean textFaltHarVarde(JTextField rutaAttKolla)
-    {
-        boolean resultat = true; 
-        
-        if(rutaAttKolla.getText().isEmpty()){
+    public static boolean textFaltHarVarde(JTextField rutaAttKolla) {
+        boolean resultat = true;
+
+        if (rutaAttKolla.getText().isEmpty()) {
             JOptionPane.showMessageDialog(null, "Inmatningsrutan är tom!");
+            rutaAttKolla.requestFocus();
             resultat = false;
         }
-        
+
         return resultat;
     }
-    
-   
+
 //Metod för att kontrollera att inmatningsrutan endast accepterar heltatsvärden 
-    
-    public static boolean isHeltal(JTextField rutaAttKolla)
-    {
-        boolean resultat= true;
-        
+    public static boolean isHeltal(JTextField rutaAttKolla) {
+        boolean resultat = true;
+
         try {
-           String inStrang = rutaAttKolla.getText();
-           Integer.parseInt(inStrang);
+            String inStrang = rutaAttKolla.getText();
+            Integer.parseInt(inStrang);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "var god ange heltal");
         }
-        catch(NumberFormatException e){
-        JOptionPane.showMessageDialog(null, "var god ange heltal");
-        }
-        
+
         return resultat;
-} 
+    }
 
 //Metod för att kontrollera att man skriver in rätt format av datum 
-     static public boolean textContainsDate(JTextField text)
-    {
-        String  regEx ="^[1-2]+[0-9]+[0-9]+[0-9-]+[0-1]+[0-9-]+[0-3]+[0-9]";
-        if(text.getText().matches(regEx))
-        {
+    static public boolean textContainsDate(JTextField text) {
+        String regEx = "^[1-2]+[0-9]+[0-9]+[0-9-]+[0-1]+[0-9-]+[0-3]+[0-9]";
+        if (text.getText().matches(regEx)) {
             return true;
         } else {
             JOptionPane.showMessageDialog(null, "Ange datum åååå-mm-dd");
         }
         return false;
     }
-    
+
+    public boolean isUsernameCorrect(JTextField txtUsername) {
+        boolean correctUsername = false;
+        try {
+            String svar = null;
+            //Skriv om denna fråga så att den fungerar med databasen
+            String fraga = "SELECT EFTERNAMN FROM LARARE WHERE EFTERNAMN = '" + txtUsername.getText() + "';";
+            svar = idb.fetchSingle(fraga);
+            if (svar == null) {
+                JOptionPane.showMessageDialog(null, "Fel användarnamn!");
+                txtUsername.requestFocus();
+            } else {
+                correctUsername = true;
+            }
+        } catch (InfException e) {
+            JOptionPane.showMessageDialog(null, "Något gick fel");
+            System.out.println("Internt felmeddelande" + e.getMessage());
+        }
+
+        return correctUsername;
+    }
+
+    public boolean isPasswordCorrect(JTextField txtUsername, String txtPassword) {
+        boolean passwordIsCorrect = true;
+
+        try {
+            String username = txtUsername.getText();
+            String password = txtPassword;
+            //Skriv om denna metod så att den fungerar med databasen
+            String correctPassword = "SELECT LARARE.LOSENORD FROM LARARE WHERE EFTERNAMN = '" + username + "';";
+            correctPassword = idb.fetchSingle(correctPassword);
+
+            if (!(password.equals(correctPassword))) {
+                passwordIsCorrect = false;
+                JOptionPane.showMessageDialog(null, "Fel lösen. Försök igen");
+            }
+
+        } catch (InfException e) {
+            JOptionPane.showMessageDialog(null, "Något gick fel");
+            System.out.println("Internt felmeddelande" + e.getMessage());
+        }
+        return passwordIsCorrect;
+    }
+
+    public boolean isAdminCorrect(JTextField txtUsername) {
+        boolean isAdmin = false;
+        try {
+            String username = txtUsername.getText();
+            String fraga = "SELECT LARARE.ADMINISTRATOR FROM LARARE WHERE EFTERNAMN = '" + username + "';";
+            String admin = idb.fetchSingle(fraga);
+            if (admin.equals("T")) {
+                isAdmin = true;
+            }
+        } catch (InfException e) {
+            JOptionPane.showMessageDialog(null, "Något gick fel");
+            System.out.println("Internt felmeddelande" + e.getMessage());
+        }
+        return isAdmin;
+    }
+
 }
